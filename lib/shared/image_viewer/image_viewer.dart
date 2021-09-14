6d6e61eb8425b0f9ac9dca49acfa760af7394943
EssaShehab <essa323371@gmail.com>
2021-09-14 16:28:19 +0300

    news Screen publish

---
+++ lib/shared/image_viewer/image_viewer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class ImageViewer extends StatefulWidget {
+  const ImageViewer({Key? key}) : super(key: key);
+  static String imageViewerScreen = "/imageViewerScreen";
+
+  @override
+  _ImageViewerState createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
