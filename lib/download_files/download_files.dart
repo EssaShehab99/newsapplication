445bc52bc4e565b2d56ff8a52a46a50aef44dd42
EssaShehab <essa323371@gmail.com>
2021-09-15 23:40:31 +0300

    image download button and cancel 1

--- lib/download_files/download_files.dart
+++ lib/download_files/download_files.dart
@@ -7,11 +7,18 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DownloadFiles extends StatefulWidget {
-  const DownloadFiles({Key? key,required  this.url,required  this.folder, this.extension = 'jpg',}) : super(key: key);
+  const DownloadFiles({
+    Key? key,
+    required this.url,
+    required this.folder,
+    this.extension = 'jpg',
+  }) : super(key: key);
 
-final  String folder ;
-  final   String url  ;
-  final   String extension  ;
+  final String folder;
+
+  final String url;
+
+  final String extension;
 
   @override
   _DownloadFilesState createState() => _DownloadFilesState();
@@ -21,25 +28,37 @@ class _DownloadFilesState extends State<DownloadFiles> {
   late DownloaderUtils options;
   late DownloaderCore core;
   late final String path;
-  double value=0.0;
+  double value = 0.0;
+  bool isDownload = false;
 
-  Future downloadLocal({required String url,required String folder, String extension = 'jpg'}) async {
+  String? localPath;
+
+  Future downloadLocal(
+      {required String url,
+      required String folder,
+      String extension = 'jpg'}) async {
     var status = await Permission.storage.status;
     if (!status.isGranted) {
       await Permission.storage.request();
     }
-
+    else return;
+    String _filePath =
+        '/storage/emulated/0/$folder/${DateTime.now()}.$extension';
     options = DownloaderUtils(
       progressCallback: (current, total) {
         final progress = (current / total);
         setState(() {
-          value=progress;
+          value = progress;
         });
         print('Downloading: $progress');
       },
-      file: File('/storage/emulated/0/$folder/${DateTime.now()}.$extension'),
+      file: File(_filePath),
       progress: ProgressImplementation(),
-      onDone: () => print('COMPLETE $path'),
+      onDone: () {
+        localPath = _filePath;
+        setState(() {});
+        print('COMPLETE $localPath');
+      },
       deleteOnCancel: true,
     );
     core = await Flowder.download(url, options);
@@ -62,27 +81,35 @@ class _DownloadFilesState extends State<DownloadFiles> {
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: CircularPercentIndicator(
-        radius: 100.0,
-        lineWidth: 10.0,
-        percent: value.toDouble(),
-        center: ElevatedButton(
-          onPressed: () {
-            downloadLocal(url: widget.url, folder: widget.folder);
-          },
-          style: ElevatedButton.styleFrom(
-              shape: CircleBorder(),
-              primary: Colors.white.withOpacity(0.5),
-              minimumSize: Size(50, 50)),
-          child: Icon(
-            Icons.download,
-            color: Colors.black45,
-          ),
-        ),
-        progressColor: Colors.orange,
-      ),
-    );
+    return localPath == null
+        ? Center(
+            child: CircularPercentIndicator(
+              radius: 50.0,
+              lineWidth: 4.0,
+              backgroundColor: Colors.black.withOpacity(0.0),
+              percent: value.toDouble(),
+              center: TextButton(
+                onPressed: () async {
+                  if (isDownload)
+                  return  core.cancel();
+                  else {
+                    isDownload = true;
+                    downloadLocal(url: widget.url, folder: widget.folder);
+                  }
+                  setState(() {});
+                },
+                style: TextButton.styleFrom(
+                    shape: CircleBorder(),
+                    backgroundColor: Colors.black.withOpacity(0.3),
+                    minimumSize: Size(50, 50)),
+                child: Icon(
+                  isDownload ? Icons.stop : Icons.download,
+                  color: Colors.white,
+                ),
+              ),
+              progressColor: Colors.orange,
+            ),
+          )
+        : Image.file(File(localPath!));
   }
 }
-
