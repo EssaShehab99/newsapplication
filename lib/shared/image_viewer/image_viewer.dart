1ef7875803dfb129885f8b85c9d4c631653d8774
EssaShehab <essa323371@gmail.com>
2021-09-15 22:41:20 +0300

    image viewer first

--- lib/shared/image_viewer/image_viewer.dart
+++ lib/shared/image_viewer/image_viewer.dart
@@ -1,6 +1,7 @@
 import 'package:flowder/flowder.dart';
 import 'package:flutter/material.dart';
 import 'package:http/src/response.dart';
+import 'package:newsapplication/download_files/download_files.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -9,105 +10,87 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
 class PhotoViewer extends StatefulWidget {
-  PhotoViewer({Key? key, required this.images,required Function(DismissDirection) this.onDismissed}) :super(key: key);
+  PhotoViewer(
+      {Key? key,
+      required this.images,
+      required Function(DismissDirection) this.onDismissed})
+      : super(key: key);
   final List<dynamic>? images;
 
   @override
   _PhotoViewerState createState() => _PhotoViewerState();
 
- final Function onDismissed ;
+  final Function onDismissed;
 }
 
 class _PhotoViewerState extends State<PhotoViewer> {
-
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: CarouselSlider(
-                items: widget.images!=null?widget.images
-                    ?.map((image) {
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: CarouselSlider(
+            items: widget.images != null
+                ? widget.images?.map(
+                    (image) {
                       return Dismissible(
-                      direction: DismissDirection.down,
-                      key: Key(image.toString()),
-                      onDismissed: (_){
-                        widget.images?.removeAt(widget.images!.indexOf(image));
-                        setState(() {
-
-                        });
-                        print(image.absolute);
-                      },
-                      child: DefaultBoxImage(images: widget.images!, image: image,download: false,),
-                    );
+                        direction: DismissDirection.down,
+                        key: Key(image.toString()),
+                        onDismissed: (_) {
+                          widget.images
+                              ?.removeAt(widget.images!.indexOf(image));
+                          setState(() {});
+                          print(image.absolute);
+                        },
+                        child: DefaultBoxImage(
+                          images: widget.images!,
+                          image: image,
+                          download: false,
+                        ),
+                      );
                     },
-                  )
-                    .toList():[],
-                options: CarouselOptions(
-                  enlargeCenterPage: true,
-                  height: 220,
-                ),
-              ),
+                  ).toList()
+                : [],
+            options: CarouselOptions(
+              enlargeCenterPage: true,
+              height: 220,
             ),
           ),
-        );
+        ),
+      ),
+    );
   }
 }
 
 class DefaultBoxImage extends StatefulWidget {
-  const DefaultBoxImage({Key? key, required this.images,required this.image,this.download=true}) : super(key: key);
+  const DefaultBoxImage(
+      {Key? key,
+      required this.images,
+      required this.image,
+      this.download = true})
+      : super(key: key);
   final List<dynamic> images;
-  final  image;
+  final image;
 
-  final bool  download;
+  final bool download;
+
   @override
   _DefaultBoxImageState createState() => _DefaultBoxImageState();
 }
 
 class _DefaultBoxImageState extends State<DefaultBoxImage> {
-  bool  download=true;
+  bool download = true;
 
   late DownloaderUtils options;
   late DownloaderCore core;
   late final String path;
 
-downloadimage() async {
-  var status = await Permission.storage.status;
-  if (!status.isGranted) {
-    await Permission.storage.request();
-  }
-
-  options = DownloaderUtils(
-    progressCallback: (current, total) {
-      final progress = (current / total) * 100;
-      print('Downloading: $progress');
-    },
-    file: File('/storage/emulated/0/200MB.jpg'),
-    progress: ProgressImplementation(),
-    onDone: () => print('COMPLETE $path' ),
-    deleteOnCancel: true,
-  );
-  core = await Flowder.download(
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
-      options);
-
-}
-  Future<void> initPlatformState() async {
-    _setPath();
-    if (!mounted) return;
-  }
-
-  void _setPath() async {
-    path = (await getExternalStorageDirectory())!.path;
-  }
 
   @override
   void initState() {
-    download=widget.download;
-    initPlatformState();
+    download = widget.download;
     super.initState();
   }
 
@@ -116,97 +99,75 @@ downloadimage() async {
     return Card(
       child: Stack(
         children: [
-          if(widget.image.runtimeType!=String||(widget.image.runtimeType==String&&download))
+          if (widget.image.runtimeType != String ||
+              (widget.image.runtimeType == String && download))
             ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) =>
-                            DefaultInteractiveImage(
-                              images: widget.images,
-                              selectedIndex:widget.images.indexOf(widget.image),
-                            )));
-              },
-              child: PhotoView(
-                disableGestures: true,
-                imageProvider: DefaultInteractiveImage.imageProvider(widget.image),
-                initialScale: PhotoViewComputedScale
-                    .covered,
-                errorBuilder: (context, error,
-                    stackTrace) =>
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                        },
-                        style: ElevatedButton
-                            .styleFrom(
-                            shape: CircleBorder(),
-                            primary: Colors.white
-                                .withOpacity(0.5),
-                            minimumSize: Size(
-                                50, 50)),
-                        child: const Icon(
-                          Icons.refresh,
-                          color: Colors.black,
-                        ),
+              borderRadius: BorderRadius.circular(10),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => DefaultInteractiveImage(
+                                images: widget.images,
+                                selectedIndex:
+                                    widget.images.indexOf(widget.image),
+                              )));
+                },
+                child: PhotoView(
+                  disableGestures: true,
+                  imageProvider:
+                      DefaultInteractiveImage.imageProvider(widget.image),
+                  initialScale: PhotoViewComputedScale.covered,
+                  errorBuilder: (context, error, stackTrace) => Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          primary: Colors.white.withOpacity(0.5),
+                          minimumSize: Size(50, 50)),
+                      child: const Icon(
+                        Icons.refresh,
+                        color: Colors.black,
                       ),
                     ),
-                loadingBuilder: (context, event) =>
-                    Center(
-                      child: SizedBox(
-                        width: 20.0,
-                        height: 20.0,
-                        child: CircularProgressIndicator(
-                          value: event == null
-                              ? 0
-                              : event
-                              .cumulativeBytesLoaded /
-                              event
-                                  .expectedTotalBytes!,
-                        ),
+                  ),
+                  loadingBuilder: (context, event) => Center(
+                    child: SizedBox(
+                      width: 20.0,
+                      height: 20.0,
+                      child: CircularProgressIndicator(
+                        value: event == null
+                            ? 0
+                            : event.cumulativeBytesLoaded /
+                                event.expectedTotalBytes!,
                       ),
                     ),
+                  ),
+                ),
               ),
             ),
-          ),
-          if(widget.image.runtimeType==String&&download==false)
-            Center(
-            child: ElevatedButton(
-              onPressed: () {
-
-                download=true;
-                setState(() {
-
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                  shape: CircleBorder(),
-                  primary: Colors.white.withOpacity(0.5),
-                  minimumSize: Size(50, 50)),
-              child: Icon(
-                Icons.download,
-                color: Colors.black,
-              ),
-            ),
-          )
+          if (widget.image.runtimeType == String && download == false)
+            DownloadFiles(url: widget.image, folder: 'Yemen Net')
         ],
       ),
     );
   }
-
-
 }
 
 class DefaultInteractiveImage extends StatefulWidget {
-  const DefaultInteractiveImage({Key? key, required this.images,required int selectedIndex}) : super(key: key);
+  const DefaultInteractiveImage(
+      {Key? key, required this.images, required int selectedIndex})
+      : super(key: key);
   final List<dynamic> images;
+
   static ImageProvider imageProvider(image) {
-    if(image.runtimeType==String)return NetworkImage(image);
-    else return FileImage(image);
+    if (image.runtimeType == String)
+      return NetworkImage(image);
+    else
+      return FileImage(image);
   }
+
   @override
   State<DefaultInteractiveImage> createState() =>
       _DefaultInteractiveImageState();
@@ -229,54 +190,61 @@ class _DefaultInteractiveImageState extends State<DefaultInteractiveImage> {
                     children: [
                       PageView(
                         scrollDirection: Axis.horizontal,
-                        children: widget.images.map((image) =>
-                            Stack(
-                              children: [
-
-                                PhotoView(
-                                  imageProvider: DefaultInteractiveImage.imageProvider(image),
-                                  initialScale: PhotoViewComputedScale.contained,
-                                  errorBuilder: (context, error, stackTrace) =>
-                                      Center(
-                                        child: ElevatedButton(
-                                          onPressed: () {
-                                            // images[images.indexOf(e)]
-                                            //     .localUrl = 'assets/images/a.png';
-                                            // setState(() {});
-                                          },
-                                          style: ElevatedButton.styleFrom(
-                                              shape: CircleBorder(),
-                                              primary: Colors.white.withOpacity(
-                                                  0.5),
-                                              minimumSize: Size(50, 50)),
-                                          child: const Icon(
-                                            Icons.refresh,
-                                            color: Colors.black,
-                                          ),
+                        children: widget.images
+                            .map(
+                              (image) => Stack(
+                                children: [
+                                  PhotoView(
+                                    imageProvider:
+                                        DefaultInteractiveImage.imageProvider(
+                                            image),
+                                    initialScale:
+                                        PhotoViewComputedScale.contained,
+                                    errorBuilder:
+                                        (context, error, stackTrace) => Center(
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          // images[images.indexOf(e)]
+                                          //     .localUrl = 'assets/images/a.png';
+                                          // setState(() {});
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            shape: CircleBorder(),
+                                            primary:
+                                                Colors.white.withOpacity(0.5),
+                                            minimumSize: Size(50, 50)),
+                                        child: const Icon(
+                                          Icons.refresh,
+                                          color: Colors.black,
                                         ),
                                       ),
-                                  loadingBuilder: (context, event) =>
-                                      Center(
-                                        child: SizedBox(
-                                          width: 20.0,
-                                          height: 20.0,
-                                          child: CircularProgressIndicator(
-                                            value: event == null
-                                                ? 0
-                                                : event.cumulativeBytesLoaded /
-                                                event.expectedTotalBytes!,
-                                          ),
+                                    ),
+                                    loadingBuilder: (context, event) => Center(
+                                      child: SizedBox(
+                                        width: 20.0,
+                                        height: 20.0,
+                                        child: CircularProgressIndicator(
+                                          value: event == null
+                                              ? 0
+                                              : event.cumulativeBytesLoaded /
+                                                  event.expectedTotalBytes!,
                                         ),
                                       ),
-                                  onTapDown: (context, details, controllerValue) {
-                                    _isVisible = !_isVisible;
-                                    setState(() {});
-                                  },
-                                  minScale: PhotoViewComputedScale.contained * 1,
-                                  maxScale: PhotoViewComputedScale.covered * 2.0,
-                                ),
-                              ],
-                            ),).toList(),
+                                    ),
+                                    onTapDown:
+                                        (context, details, controllerValue) {
+                                      _isVisible = !_isVisible;
+                                      setState(() {});
+                                    },
+                                    minScale:
+                                        PhotoViewComputedScale.contained * 1,
+                                    maxScale:
+                                        PhotoViewComputedScale.covered * 2.0,
+                                  ),
+                                ],
+                              ),
+                            )
+                            .toList(),
                         allowImplicitScrolling: true,
                       ),
                       AnimatedContainer(
@@ -327,4 +295,3 @@ class _DefaultInteractiveImageState extends State<DefaultInteractiveImage> {
     );
   }
 }
-
